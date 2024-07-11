7e306308f3fcbe770ef256466984b5a1051b61c3
JinalEmpiricTech <jinal@empiricinfotech.com>
2024-07-11 19:11:02 +0530

    create user model

--- healthcare_appointment/user_management/models.py
+++ healthcare_appointment/user_management/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+from role_management.models import Role
 
 # Create your models here.
-class Role(models.Model):
-    title = models.CharField(max_length=25, unique=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-    deleted_at = models.DateTimeField(auto_now=True)
 
+class User(AbstractUser):
+    role = models.ForeignKey(Role, on_delete=models.CASCADE, null=True, blank=True)
 
+    def __str__(self):
+        return self.first_name + ' ' + self.last_name
