8834ecc5b881c3932d6f6beb60da38a128b1cf02
JinalEmpiricTech <jinal@empiricinfotech.com>
2024-07-11 19:12:52 +0530

    user urls

---
+++ healthcare_appointment/user_management/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.UserCRUDAPIView.as_view(), name="usercrud"),
+    path('login/', views.UserLoginView.as_view(), name="login"),
+    path('register/', views.UserRegistrationView.as_view(), name="login"),
+    path('<int:id>/', views.UserCRUDAPIView.as_view(), name="usercruddetail"),
+    path('password-reset/', views.PasswordResetRequestView.as_view(), name='password_reset'),
+    path('reset-password-confirm/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+]
