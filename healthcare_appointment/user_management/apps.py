fad8079f6487a42484b246af52f711b805caf356
JinalEmpiricTech <jinal@empiricinfotech.com>
2024-07-12 15:39:18 +0530

    add ready method to use signals

--- healthcare_appointment/user_management/apps.py
+++ healthcare_appointment/user_management/apps.py
@@ -1,6 +1,10 @@
-from django.apps import AppConfig
+# user_management/apps.py
 
+from django.apps import AppConfig
 
 class UserManagementConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'user_management'
+    
+    def ready(self):
+        import user_management.signals  # Import signals module for registration
