f1f0e0c96f7fb23d68ce8e046941907334aded54
JinalEmpiricTech <jinal@empiricinfotech.com>
2024-07-12 15:38:57 +0530

    Register DoctorProfile, PatientProfile, Availability models

--- healthcare_appointment/user_management/admin.py
+++ healthcare_appointment/user_management/admin.py
@@ -1,7 +1,23 @@
 from django.contrib import admin
-from .models import User
+from .models import User, DoctorProfile, PatientProfile, Availability
 
 # Register your models here.
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ['id', 'role', 'first_name', 'last_name', 'username', 'email', 'is_active', 'date_joined']
+
+
+@admin.register(DoctorProfile)
+class DoctorProfileAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'specialty', 'qualifications']
+
+
+@admin.register(PatientProfile)
+class PatientProfileAdmin(admin.ModelAdmin):
+    list_display = ['__str__']
+
+
+@admin.register(Availability)
+class AvailabilityAdmin(admin.ModelAdmin):
+    list_display = ['__str__', 'day_of_week', 'start_time', 'end_time']
+
