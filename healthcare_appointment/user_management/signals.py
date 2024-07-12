a3cc7e6529035ef931ce719ac45915a47da872ee
JinalEmpiricTech <jinal@empiricinfotech.com>
2024-07-12 15:40:15 +0530

    create create_user_profile and save_user_profile signal

--- healthcare_appointment/user_management/signals.py
+++ healthcare_appointment/user_management/signals.py
@@ -1,18 +1,41 @@
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from .models import User, DoctorProfile, PatientProfile
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ObjectDoesNotExist
+from .models import DoctorProfile, PatientProfile
+
+User = get_user_model()
 
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        if instance.role == 'doctor':
-            DoctorProfile.objects.create(user=instance)
-        elif instance.role == 'patient':
-            PatientProfile.objects.create(user=instance)
-
+        role = getattr(instance, 'role', None)
+        if role.title == 'doctor':
+            try:
+                DoctorProfile.objects.create(user=instance)
+            except Exception as e:
+                print(f"Error creating DoctorProfile: {e}")
+        elif role.title == 'patient':
+            try:
+                PatientProfile.objects.create(user=instance)
+            except Exception as e:
+                print(f"Error creating PatientProfile: {e}")
+    
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    if instance.role == 'doctor':
-        instance.doctor_profile.save()
-    elif instance.role == 'patient':
-        instance.patient_profile.save()
+    role = getattr(instance, 'role', None)
+    if role.title == 'doctor':
+        try:
+            instance.doctor_profile.save()
+        except ObjectDoesNotExist:
+            print("DoctorProfile does not exist for this user.")
+        except Exception as e:
+
+            print(f"Error saving DoctorProfile: {e}")
+    elif role.title == 'patient':
+        try:
+            instance.patient_profile.save()
+        except ObjectDoesNotExist:
+            print("PatientProfile does not exist for this user.")
+        except Exception as e:
+            print(f"Error saving PatientProfile: {e}")
