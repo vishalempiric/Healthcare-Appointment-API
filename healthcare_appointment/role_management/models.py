27053eb255218ba30361a44f1d7101465ca3d098
JinalEmpiricTech <jinal@empiricinfotech.com>
2024-07-12 11:52:33 +0530

    Add new role choice and deleted_at field

--- healthcare_appointment/role_management/models.py
+++ healthcare_appointment/role_management/models.py
@@ -5,10 +5,12 @@ class Role(models.Model):
     ROLE_CHOICES = [
         ('doctor', 'Doctor'),
         ('patient', 'Patient'),
+        ('admin', 'Admin'),
     ]
     title = models.CharField(choices=ROLE_CHOICES, max_length=25, unique=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    deleted_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
