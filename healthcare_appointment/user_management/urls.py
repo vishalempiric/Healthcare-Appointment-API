0c98841752784810003c4ff82ecf27e56b4bee65
JinalEmpiricTech <jinal@empiricinfotech.com>
2024-07-12 15:41:08 +0530

    add new url for profile

--- healthcare_appointment/user_management/urls.py
+++ healthcare_appointment/user_management/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     path('<int:id>/', views.UserCRUDAPIView.as_view(), name="usercruddetail"),
     path('password-reset/', views.PasswordResetRequestView.as_view(), name='password_reset'),
     path('reset-password-confirm/<uidb64>/<token>/', views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
 ]
